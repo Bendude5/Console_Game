5e77c525d9c6972724c845aa5860f38cc03d689d
JWright667 <jacob.wright3@mail.bcu.ac.uk>
2023-02-16 13:23:36 +0000

    Hub Updated and arcade 3 started
    the hub has been updated to work with all 3 levels as well as adding UI elements.
    the 3rd arcade stage has also begun

---
+++ Console_Game/Assets/Scripts/HUB/Enter_Arcade.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enter_Arcade : MonoBehaviour
+{
+    public GameObject arcade1;
+    public GameObject arcade2;
+    public GameObject arcade3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        switch (other.name)
+        {
+            case "Arcade_Machine_1":
+                if(Input.GetKey(KeyCode.E))
+                {
+                    arcade1.GetComponent<Animator>().SetInteger("Anim_Number", 1);
+                    Debug.Log("Entered arcade 1");
+                }
+                break;
+
+            case "Arcade_Machine_2":
+                if (Input.GetKey(KeyCode.E))
+                {
+                    arcade2.GetComponent<Animator>().SetInteger("Anim_Number", 1);
+                    Debug.Log("Entered arcade 2");
+                }
+                break;
+
+            case "Arcade_Machine_3":
+                if (Input.GetKey(KeyCode.E))
+                {
+                    arcade3.GetComponent<Animator>().SetInteger("Anim_Number", 1);
+                    Debug.Log("Entered arcade 3");
+                }
+                break;
+        }
+    }
+
+}
--- Console_Game/Assets/Scripts/Movement.cs
+++ Console_Game/Assets/Scripts/Movement.cs
@@ -5,31 +5,62 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public CharacterController controller;
-
     public Animator playerAnim;
 
+    private Vector3 direction;
+
     public float speed = 6f;
+    public float rotationspeed = 90f;
+    private float gravity = -20f; 
 
     public float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
+    float turnSmoothVelocity;   
+
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
 
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        direction.x = horizontal * speed;
+        direction.z = vertical * speed;
 
         if (direction.magnitude >= 0.1f)
         {
+
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
             playerAnim.SetFloat("Speed", 1.0f);
-            controller.Move(direction * speed * Time.deltaTime);
+
+            
+            controller.Move(direction * Time.deltaTime);
+
         }
         else
         {
             playerAnim.SetFloat("Speed", 0.0f);
         }
+
+
+        //float horizontal = Input.GetAxis("Horizontal");
+        //float vertical = Input.GetAxis("Vertical");
+
+        //if (controller.isGrounded)
+            //{
+            //    direction = transform.forward * speed * vertical;
+            //    turnVelocity = transform.up * rotationspeed * horizontal;            
+            //}
+            //direction.y += gravity * Time.deltaTime;
+        
+        //controller.Move(moveVelocity * Time.deltaTime);
+        //transform.Rotate(turnVelocity * Time.deltaTime);
     }
+
+
 }
