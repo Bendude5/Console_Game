3cc6d6926c72878cb79db3e9d94e22f77eed62c0
Ben <56559770+Bendude5@users.noreply.github.com>
2023-04-20 12:39:57 +0100

    Merge test

---
+++ Console_Game/Assets/ProgressManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ CreateAssetMenu (fileName = "Debug Progress")]
+public class ProgressManager : ScriptableObject
+{
+    public int score;
+
+}
