fb89e0b91863c9a0f393180f1f2d5bcad892646e
JWright667 <jacob.wright3@mail.bcu.ac.uk>
2023-04-21 00:05:02 +0100

    Various Level Updates
    - Intro Cutscene Added
    - Snow Level Added
    - Cloud Area Updated
    - Lava Boulder Updated

---
+++ Console_Game/Assets/CutsceneLVL3.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CutsceneLVL3 : MonoBehaviour
+{
+    public Camera mainCam;
+    public Camera introCam;
+
+    public Animator anim;
+
+    Console_Game playerControls;
+
+    void Awake()
+    {
+        playerControls = new Console_Game();
+
+        playerControls.Player.Attack.performed += ctx => finishCutscene();
+    }
+
+    void OnEnable()
+    {
+        playerControls.Player.Enable();
+    }
+
+    void OnDisable()
+    {
+        playerControls.Player.Disable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        anim.Play("Scene 3 Intro", 0, 0);
+        mainCam.gameObject.SetActive(false);
+        introCam.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            this.finishCutscene();
+        }
+    }
+
+    //This function does exactly what it says it does
+    public void finishCutscene()
+    {
+        mainCam.gameObject.SetActive(true);
+        introCam.gameObject.SetActive(false);
+    }
+}
