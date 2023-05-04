aaf33586b07e25b78f58b82f973c21d59e098e63
JWright667 <91148228+JWright667@users.noreply.github.com>
2023-05-04 15:12:58 +0100

    Spawn Tweaks

--- Console_Game/Assets/Scripts/Spawn_Player.cs
+++ Console_Game/Assets/Scripts/Spawn_Player.cs
@@ -1,41 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Spawn_Player : MonoBehaviour
 {
     [SerializeField]
     ProgressManager progress;
 
+    public GameObject Player;
+
+    public GameObject spawnEnter;
+    public GameObject spawnLevel1;
+    public GameObject spawnLevel2;
+    public GameObject spawnBoss;
+
     public bool inHUB;
 
-    public void spawn()
+    //public void Start()
+    //{
+    //    //if (inHUB == true)
+    //    //{
+    //    //    if (progress.LastLevel == 0)
+    //    //    {
+
+    //    //    }
+
+    //    //    else if (progress.LastLevel == 1)
+    //    //    {
+
+    //    //    }
+
+    //    //    else if (progress.LastLevel == 2)
+    //    //    {
+
+    //    //    }
+
+    //    //    else if (progress.LastLevel == 3)
+    //    //    {
+
+    //    //    }
+
+    //    SceneManager.sceneLoaded += OnSceneLoaded;
+
+    //}
+
+    void Awake()
     {
-        if (inHUB == true)
-        {
-            if (progress.LastLevel == 0)
+        
+            switch (progress.LastLevel)
             {
-                gameObject.transform.position = new Vector3(529, 1, 453);
-                Debug.Log("Area 1");
-            }
+                case 0:
+                    Player.transform.position = spawnEnter.transform.position;
+                    Debug.Log("Main Spawn");
+                    break;
+                case 1:
+                    Player.transform.position = spawnLevel1.transform.position;
+                    Debug.Log("Arcade 1");
+                    break;
+                case 2:
+                    Player.transform.position = spawnLevel2.transform.position;
+                    Debug.Log("Arcade 2");
+                    break;
+                case 3:
+                    Player.transform.position = spawnBoss.transform.position;
+                    Debug.Log("Boss Area");
+                    break;
 
-            else if (progress.LastLevel == 1)
-            {
-                gameObject.transform.position = new Vector3(518, 1, 465);
-                Debug.Log("Area 2");
             }
-
-            else if (progress.LastLevel == 2)
-            {
-                gameObject.transform.position = new Vector3(535, 2, 489);
-                Debug.Log("Area 3");
-            }
-
-            else if (progress.LastLevel == 3)
-            {
-                gameObject.transform.position = new Vector3(504, 1, 506);
-                Debug.Log("Boss Area");
-            }
-        }
+        
     }
+ 
+      
+    
 }
